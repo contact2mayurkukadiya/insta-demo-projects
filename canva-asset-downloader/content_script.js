a361ffa34e9c291e9be97b14bf91befa86bf05a2
Mayur <contact2mayurkukadiya@gmail.com>
2023-10-14 19:39:19 +0530

    1. add multi zip downloading feature

--- canva-asset-downloader/content_script.js
+++ canva-asset-downloader/content_script.js
@@ -1,14 +1,44 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    // if (request.action == "loadLibrary") loadLibrary(sendResponse);
+    if (request.action == "download") downloadImagesInContainer();
 });
 
-function loadLibrary(sendResponse) {
-    // const script = document.createElement('script');
-    // script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.6.0/jszip.min.js'; // Replace with the latest version from the CDN
-    // script.type = 'text/javascript';
-    // document.getElementsByTagName('head')[0].appendChild(script);
-    // sendResponse({
-    //     code: 200,
-    //     action: "Library Loaded",
-    // });
+function downloadImagesInContainer() {
+    const containerSelector = ".uPeMFQ";
+    const containers = document.querySelectorAll(containerSelector);
+    if (!containers || containers.length <= 0) {
+        console.error("Container not found!");
+        return;
+    }
+
+    containers.forEach(container => {
+
+        const images = container.querySelectorAll("img");
+        const zip = new JSZip();
+        const imgFolder = zip.folder("images");
+
+        images.forEach((image, index) => {
+            const imageUrl = image.src;
+            fetch(imageUrl)
+                .then((response) => response.blob())
+                .then((blob) => {
+                    const contentType = blob.type;
+                    var extension = contentType.split('/')[1];
+                    if (extension == 'svg+xml') {
+                        extension = extension.split("+")[0];
+                    }
+
+                    imgFolder.file(`image_${index}.${extension}`, blob);
+                    if (index === images.length - 1) {
+                        zip.generateAsync({ type: "blob" }).then((content) => {
+                            const url = window.URL.createObjectURL(content);
+                            const a = document.createElement("a");
+                            a.href = url;
+                            a.download = `images.zip`;
+                            a.click();
+                            window.URL.revokeObjectURL(url);
+                        });
+                    }
+                });
+        });
+    })
 }
